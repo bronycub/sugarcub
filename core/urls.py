23d9b63e2721f34c2a6548c7afa1a64b9b72361b
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2014-12-05 13:58:18 +0100

    Fix auth links

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^map$',     'core.views.map',     name = 'map'),
     url(r'^friends$', 'core.views.friends', name = 'friends'),
 
-	url(r'^login$',  'django.contrib.auth.views.login',  {'template_name': 'auth/login.html', 'current_app': 'core'},  name = 'login'),
-	url(r'^logout$', 'django.contrib.auth.views.logout', {'template_name': 'auth/logout.html', 'next_page': 'home'}, name = 'logout'),
+	url(r'^login$',   'django.contrib.auth.views.login',  {'template_name': 'auth/login.html',  'current_app': 'core'},    name = 'login'),
+	url(r'^logout$',  'django.contrib.auth.views.logout', {'template_name': 'auth/logout.html', 'next_page': 'core:home'}, name = 'logout'),
 )
 
