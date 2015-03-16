0db4c9c759283b1aa30561cbde4815bf85b44530
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2015-03-16 20:14:41 +0100

    damn tabspace …

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$',        'core.views.home',    name = 'home'),
+    url(r'^$',		  'core.views.home',	name = 'home'),
 
-    url(r'^agenda$',  'core.views.home',    name = 'agenda'),
-    url(r'^map$',     'core.views.map',     name = 'map'),
+    url(r'^agenda$',  'core.views.home',	name = 'agenda'),
+    url(r'^map$',	  'core.views.map',		name = 'map'),
     url(r'^friends$', 'core.views.friends', name = 'friends'),
 )
 
