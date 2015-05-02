c8244c86b79436c1ffe00491d1774860152609d5
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2015-05-02 12:45:27 +0200

    Some layout enhancements, you must be logged in to see comments

--- agenda/urls.py
+++ agenda/urls.py
@@ -3,15 +3,15 @@ from .                        import views, models
 from endless_pagination.views import AjaxListView
 
 urlpatterns = patterns('',
-    url(
-        regex = r'^$',
-        view  = AjaxListView.as_view(model = models.Event),
-        name  = 'list',
-    ),
-    url(
-        regex = r'^(?P<event_id>[0-9]+)/comments',
-        view  = views.CommentAjaxListView.as_view(),
-        name  = 'comment_list',
-    ),
+	url(
+		regex = r'^$',
+		view  = AjaxListView.as_view(model = models.Event),
+		name  = 'list',
+	),
+	url(
+		regex = r'^(?P<event_id>[0-9]+)/comments$',
+		view  = views.CommentAjaxListView.as_view(),
+		name  = 'comment_list',
+	),
 )
 
