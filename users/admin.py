83488cb6895686d443cb3af8bcc4348ec3fe1c51
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2015-06-29 20:28:43 +0200

    Add a registration step, users must be enabled by an administrator

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .              import models
 
-admin.site.register(models.Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+
+    def enable_profile(self, request, queryset):
+        rows_updated = queryset.update(enabled = True)
+        self.message_user(request, '%s profiles were enabled.', rows_updated)
+
+    def disable_profile(self, request, queryset):
+        rows_updated = queryset.update(enabled = False)
+        self.message_user(request, '%s profiles were disabled.', rows_updated)
+
+    enable_profile.short_description  = 'Enable the selected profiles.'
+    disable_profile.short_description = 'Disable the selected profiles.'
+
+    actions = [enable_profile, disable_profile]
+
+
+admin.site.register(models.Profile, ProfileAdmin)
 admin.site.register(models.Pony)
 admin.site.register(models.Url)
