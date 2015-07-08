e557faf23eed4cff0725f7051c715f580c8120b3
BCazaubon <baptiste.cazaubon@gmail.com>
2015-07-08 19:04:29 +0200

    Translation and add of a prefix for users page

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,27 @@
-from django.conf.urls     import patterns, url
-from django.views.generic import TemplateView
-from .views               import RegistrationView
+from django.conf.urls     		import patterns, url, include
+from django.views.generic 		import TemplateView
+from .views               		import RegistrationView
+from django.conf.urls.i18n 		import i18n_patterns
+from django.utils.translation	import ugettext_lazy 			as _
 
 urlpatterns = patterns('',
-    url(r'^members$', 'users.views.members', name = 'members'),
-    url(r'^profile$', 'users.views.profile', name = 'profile'),
-    url(r'^register/$',
-        RegistrationView.as_view(),
-        name='registration_register'
-    ),
-    url(r'^register/informations$',
-        TemplateView.as_view(template_name = 'registration/informations.html'),
-        name = 'pre_register'),
+	# Membres
+    url(r'^members$',                          'users.views.members',                                                   name = 'members'),
+    # Profil
+    url(r'^profile$',                          'users.views.profile',                                                   name = 'profile'),
+    # Inscription
+    url(r'^register$',                         RegistrationView.as_view(),                                              name='registration_register' ),
+    # Information
+    url(r'^register/informations$',            TemplateView.as_view(template_name = 'registration/informations.html'),  name = 'pre_register'),
+
+    url(_(r'^en/'), include(patterns('',
+    	# Membres
+	    url(_(r'^members$'),                   'users.views.members',                                                   name = 'members'),
+	    # Profil
+	    url(_(r'^profile$'),                   'users.views.profile',                                                   name = 'profile'),
+	    # Inscription
+	    url(_(r'^register$'),                  RegistrationView.as_view(),                                              name='registration_register' ),
+	    # Information
+	    url(_(r'^register/informations$'),     TemplateView.as_view(template_name = 'registration/informations.html'),  name = 'pre_register'),
+    ))),
 )
