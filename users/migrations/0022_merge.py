dd8b6fa10adba238d0c82987906cd6efb9604349
BCazaubon <Baptiste.cazaubon@gmail.com>
2015-12-01 19:13:58 +0000

    Merge 22

---
+++ users/migrations/0022_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0021_merge'),
+        ('users', '0017_auto_20151102_1535'),
+    ]
+
+    operations = [
+    ]
