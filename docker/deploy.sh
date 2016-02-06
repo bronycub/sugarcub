f859ace49f24cf9943b4a4bbd8e7f2894bd86d31
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2016-02-06 21:39:43 +0100

    WIP, converting build and deploy to docker-compose

---
+++ docker/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -x
+
+if [ $# -ge 1 ] ; then
+	COMMIT_ID="$1"
+else
+	echo 'No commit id was given, nothing to deploy'
+	exit 1
+fi
+
+if [ -L $0 ] ; then
+    DIR=$(dirname $(readlink -f $0))
+else
+    DIR=$(dirname $0)
+fi
+
+cd "$DIR"
+
+docker-compose -p sugarcub build --force-rm
+systemctl reload sugarcub
---
+++ docker/run-deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -x
+
+if [ $# -ge 1 ] ; then
+	COMMIT_ID="$1"
+else
+	echo 'No commit id was given, nothing to deploy'
+	exit 1
+fi
+
+USER='root'
+HOST='localhost'
+BASE_DIR='/var/www/sugarcub'
+
+if [ -L $0 ] ; then
+    DIR=$(dirname $(readlink -f $0))
+else
+    DIR=$(dirname $0)
+fi
+
+cd "$DIR"
+
+ssh $USER@$HOST "mkdir -p $BASE_DIR"
+scp ./deploy.sh $USER@$HOST:"$BASE_DIR"
+scp ../docker-compose.prod.yml $USER@$HOST:"$BASE_DIR/docker-compose.yml"
+ssh $USER@$HOST "chmod u+x $BASE_DIR/deploy.sh"
+ssh $USER@$HOST "$BASE_DIR"/deploy.sh "$COMMIT_ID"
