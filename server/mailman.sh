15353602ab60c218c55390cc7e286be6b4d404e3
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2016-02-06 19:40:38 +0100

    WIP, converting build and deploy to docker-compose

--- server/mailman.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-trap '/shared/mailman/bin/mailman stop ; exit $?' TERM
-
-rm /shared/mailman/var/locks/*
-cd /shared/mailman
-/shared/mailman/bin/mailman start &
-
-/uwsgi-build/uwsgi /shared/uwsgi-mailman.ini
--- server/postfix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-trap 'postfix stop ; exit $?' TERM
-
-postfix start &
-
-/usr/local/bin/syslog-stdout.py
