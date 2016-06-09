116e2c1e23bae7efb42e42eb1c671f551f000cff
Heuzef <contact@heuzef.com>
2016-06-09 15:03:45 +0200

    syntaxt corrections

--- agenda/models.py
+++ agenda/models.py
@@ -1,7 +1,7 @@
 from   django.db                  import models
 from   django.contrib.auth.models import User
 from   django.core.urlresolvers   import reverse
-from django.utils.translation     import ugettext_lazy as _
+from   django.utils.translation   import ugettext_lazy as _
 import ics
 
 
