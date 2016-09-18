e1d7d7fcd7cff8c27a6d710a24b7cb9b895be922
Heuzef <contact@heuzef.com>
2016-09-18 17:48:31 +0200

    Initialisation du module BBBFF
    Création de la 1er vue et du 1er template. Fiou.

--- bbbff/urls.py
+++ bbbff/urls.py
@@ -1,4 +1,11 @@
-# from django.conf.urls import url
+from django.conf.urls         import url
+from .                        import views, models
+from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = [
+    url(
+        regex = r'^$',
+        view  = views.medias_category,
+        name  = 'medias_category',
+    ),
 ]
--- bbbff/views.py
+++ bbbff/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.shortcuts               import render
+from django.core.urlresolvers       import reverse_lazy
+from django.views.generic.edit      import CreateView, UpdateView
+from el_pagination.views            import AjaxListView
+from django.http                    import HttpResponse
+from django.shortcuts               import redirect
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators        import method_decorator
+import json
+
+def medias_category(request):
+
+    return render(request, 'medias_category.html')
